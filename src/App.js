65f46d3c48431249ae7eaf2176bc2735491f0d85
smtkesici <170202013@kocaeli.edu.tr>
2021-05-11 01:37:06 +0300

    add card

--- src/App.js
+++ src/App.js
@@ -29,10 +29,33 @@ const App = () => {
 
   return (
     <div className="App">
-      <form encType="multipart/form-data">
-        <input onChange={onChangeFile} type="file" name="photo" />
-      </form>
-      <AutoUploadImage image={newImage}></AutoUploadImage>
+      <div className="ml-5 d-flex flex-row">
+        <img
+          style={{ height: "10%", width: "10%" }}
+          alt="blabla"
+          src="http://max-image-caption-generator-web-app.codait-prod-41208c73af8fca213512856c7a09db52-0000.us-east.containers.appdomain.cloud/static/img/codait-logo.jpg"
+        ></img>
+        <span className="ml-4" style={{ fontSize: 36 }}>
+          Image Caption Generator
+        </span>
+        <div style={{ height: "15%", width: "15%" }} className="card p-1 ml-4">
+          <form className="form-group" encType="multipart/form-data">
+            <input
+              className="form-control-file"
+              onChange={onChangeFile}
+              type="file"
+              name="photo"
+            />
+          </form>
+          <AutoUploadImage image={newImage}></AutoUploadImage>
+        </div>
+      </div>
+      <div className="card mt-4 p-4" style={{ width: "32rem" }}>
+        <img className="card-img-top" alt="..."></img>
+        <div className="card-body">
+          <p className="card-text">blabla</p>
+        </div>
+      </div>
     </div>
   );
 };
